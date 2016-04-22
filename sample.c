4cf1acc6ae888199d176390f9c199085e13099e0
DearRabbit <DearRabbit@users.noreply.github.com>
2016-04-22 16:16:34 +0800

    missing Q in sample.c

--- sample.c
+++ sample.c
@@ -1,11 +1,13 @@
 #include "mouse.h"
 #include <stdio.h>
 
-#define MOUSE_HOST "10.110.33.227"
+#define MOUSE_HOST "localhost"
 #define MOUSE_PORT 10659
 
 int main(){
     printf("hello!\n");
-	mouse_init(10, MOUSE_HOST,MOUSE_PORT);
-	mouse_login("abcdefghijklmnoprstuvwxyzabcdef");
+	mouse_init(10, MOUSE_HOST, MOUSE_PORT);
+	mouse_login("abcdefghijklmnopqrstuvwxyzabcdef");
+	mouse_logout();
+	return 0;
 }
