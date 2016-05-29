31d4a68026086ebe413b5e7349ce2fe2371714ac
DearRabbit <DearRabbit@users.noreply.github.com>
2016-05-29 19:05:40 +0800

    rename var

--- py/recv_control.py
+++ py/recv_control.py
@@ -5,7 +5,7 @@ import httplib, urllib
 
 httpClient = None
 try:
-    send_control_data = {
+    recv_control_data = {
         'auth_id': 3,
         'auth_key': 'a04e820a8c636ba1eb121f6463d418ff',
         'device_id': 3,
@@ -16,7 +16,7 @@ try:
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
     httpClient = httplib.HTTPConnection("fat.fatmou.se", 80, timeout=30)
 
-    params = urllib.urlencode(send_control_data)
+    params = urllib.urlencode(recv_control_data)
     httpClient.request("POST", "/api/control", params, headers)
 
     response = httpClient.getresponse()
