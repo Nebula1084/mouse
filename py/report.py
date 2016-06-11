9a2991459d9ad07f929f9b3dd45b26a92ff97fd5
DearRabbit <DearRabbit@users.noreply.github.com>
2016-06-11 23:57:35 +0800

    use fat

--- py/report.py
+++ py/report.py
@@ -6,16 +6,15 @@ import httplib, urllib
 httpClient = None
 try:
     report_data = {
-        'auth_id': 1,
-        'auth_key': '1eecf1c5e848ff42d88e5599b3e8dfc0',
-        'device_id': 1,
-        'report_id': 1,
-        'payload': '{"field0": "4","field1": "0.4"}'
+        'auth_id': 3,
+        'auth_key': 'a04e820a8c636ba1eb121f6463d418ff',
+        'device_id': ,
+        'report_id': ,
+        'payload': '{"number": "10","field1": "0.4"}'
     }
 
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-    # httpClient = httplib.HTTPConnection("fat.fatmou.se", 80, timeout=30)
-    httpClient = httplib.HTTPConnection("106.2.111.144", 80, timeout=30)
+    httpClient = httplib.HTTPConnection("fat.fatmou.se", 80, timeout=30)
 
     params = urllib.urlencode(report_data)
     httpClient.request("POST", "/api/report", params, headers)
